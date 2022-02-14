489700b28f038fcb75880f161de950a5d6d2e492
Break Yang <breakds@gmail.com>
2022-02-14 15:03:43 -0800

    Set CUBLAS_WORKSPACE_CONFIG for deterministic algorithm in pytorch

--- nix/pkgs/alf-dev-shell/default.nix
+++ nix/pkgs/alf-dev-shell/default.nix
@@ -61,5 +61,10 @@ in mkShell rec {
   # the Alf python dev environment.
   shellHook = ''
     export PS1="$(echo -e '\u${pythonIcon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+
+    # We need this when deterministic algorithm is enabled in pytorch.
+    # See https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+    # This will increase library footprint in GPU memory by approximately 24MiB
+    CUBLAS_WORKSPACE_CONFIG=:4096:8
   '';
 }
