14501593b84a337a28e45007ee020813fdd76dfc
Break Yang <breakds@gmail.com>
2022-03-12 22:25:20 -0800

    Jupyter is working now

--- nix/pkgs/alf-dev-shell/default.nix
+++ nix/pkgs/alf-dev-shell/default.nix
@@ -35,7 +35,7 @@ let pythonForAlf = python3.withPackages (pyPkgs: with pyPkgs; [
       # torchtext (0.9.1)
       
       # Dev only packages
-      # jupyterlab ipywidgets ipydatawidgets
+      jupyterlab ipywidgets
       matplotlib tqdm
       sphinx_rtd_theme
       yapf
