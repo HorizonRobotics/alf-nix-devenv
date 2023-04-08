7ce753e141a3c3fbffa3caf4d0583733cdb7a63b
Break Yang <breakds@gmail.com>
2023-04-08 08:17:50 -0700

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
     # branch = PR/breakds/alf_packaged
     alf.url = "github:HorizonRobotics/alf/PR/breakds/alf_packaged";
     alf.inputs.nixpkgs.follows = "nixpkgs";
-    alf.inputs.ml-pkgs.follows = "ml-pkgs";
 
     unitree-go1-sdk.url = "git+ssh://git@github.com/HorizonRoboticsInternal/unitree-go1-sdk";
     unitree-go1-sdk.inputs.nixpkgs.follows = "nixpkgs";
