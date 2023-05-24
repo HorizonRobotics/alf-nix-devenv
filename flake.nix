cb288c23962bc27f0b326e981f6152894d30979c
Break Yang <breakds@gmail.com>
2023-05-24 13:43:36 -0700

    [Temp] docker

--- flake.nix
+++ flake.nix
@@ -64,5 +64,34 @@
           useLegacyMujoco = true;
         };
       };
+
+      packages.hobot-runtime = pkgs.dockerTools.buildImage {
+        name = "redis";
+        tag = "latest";
+
+        fromImage = someBaseImage;
+        fromImageName = null;
+        fromImageTag = "latest";
+
+        copyToRoot = pkgs.buildEnv {
+          name = "image-root";
+          paths = [ pkgs.redis ];
+          pathsToLink = [ "/bin" ];
+        };
+
+        runAsRoot = ''
+          #!${pkgs.runtimeShell}
+          mkdir -p /data
+        '';
+        
+        config = {
+          Cmd = [ "/bin/redis-server" ];
+          WorkingDir = "/data";
+          Volumes = { "/data" = { }; };
+        };
+
+        diskSize = 1024;
+        buildVMMemorySize = 512;        
+      };
     });
 }
