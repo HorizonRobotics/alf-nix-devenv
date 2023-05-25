cb1329ff259eb4b5701d6b73934dcc83e4d3530b
Break Yang <breakds@gmail.com>
2023-05-24 19:49:44 -0700

    Split the runtime-base image out

--- nix/pkgs/hobot-runtime/default.nix
+++ nix/pkgs/hobot-runtime/default.nix
@@ -10,34 +10,51 @@
 , openssh
 , curl
 , cacert
+, lsd
 }:
 
-dockerTools.buildImage {
+let runtime-base = dockerTools.buildLayeredImage {
+      name = "runtime-base";
+      tag = "latest";
+      created = "now";
+
+      contents = [
+        bash
+        git
+        iputils
+        coreutils
+        findutils
+        fd
+        openssl
+        openssh
+        curl
+      ];
+
+      config = {
+        Cmd = [ "/bin/bash" ];
+        Env = [
+          # The following environment variables makes it possible for the docker
+          # container to access internet with SSL/HTTPS.
+          "GIT_SSL_CAINFO=${cacert}/etc/ssl/certs/ca-bundle.crt"
+          "SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt"
+        ];
+      };
+    };
+
+in dockerTools.buildImage {
   name = "hobot-runtime";
   tag = "latest";
   created = "now";
 
+  fromImage = runtime-base;
+
   copyToRoot = buildEnv {
-    name = "root-environment";
-    paths = [
-      bash
-      git
-      iputils
-      coreutils
-      findutils
-      fd
-      openssl
-      openssh
-      curl
-    ];
+    name = "runtime-base-env";
+    paths = [ lsd ];
     pathsToLink = [ "/bin" ];
   };
 
   config = {
     Cmd = [ "/bin/bash" ];
-    Env = [
-      "GIT_SSL_CAINFO=${cacert}/etc/ssl/certs/ca-bundle.crt"
-      "SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt"
-    ];
-  };        
+  };
 }
