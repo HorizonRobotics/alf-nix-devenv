0bc1c88e5d77ff005cf5810f8aca0ee7bbc9eb3b
Break Yang <breakds@gmail.com>
2024-07-12 10:13:27 -0700

    [tmp]

--- nix/dockers/hobot.nix
+++ nix/dockers/hobot.nix
@@ -1,23 +1,53 @@
 { dockerTools
 , mkShell
+, stdenv
+, buildEnv
 , python3
+, bashInteractive
+, iputils
+, coreutils
+, findutils
 }:
 
-let python-env = python3.withPackages (p: with p; [
-      alf
+
+let barebone = dockerTools.buildLayeredImage {
+      name = "barebone";
+      tag = "latest";
+
+      contents = [
+        bashInteractive
+        iputils
+        coreutils
+        findutils
+      ] ++ (with dockerTools; [
+        caCertificates
+        usrBinEnv
+        binSh
+        fakeNss
+      ]);
+    };
+
+    python-env = python3.withPackages (p: with p; [
       numpy
     ]);
 
-    devShell = mkShell {
-      name = "hobot";
-      packages = [
-        python-env
-      ];
-    };
+in dockerTools.buildLayeredImage {
+  name = "hobot-cuda";
+  tag = "latest";
+  created = "now";
+  maxLayers = 512;
 
-in dockerTools.buildNixShellImage {
-  name = "hobot-docker";
-  tag = "2024.07.11";
+  fromImage = barebone;
 
-  drv = devShell;
+  contents = [
+    python-env
+  ];
+
+  config = {
+    Env = [
+      "NVIDIA_VISIBLE_DEVICES=all"
+      "NVIDIA_DRIVER_CAPABILITIES=all"
+      "LD_LIBRARY_PATH=/usr/lib64/"
+    ];
+  };
 }
